00e5f7b5ab612e0b02d4f964f32f3fa005084e39
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-02-25 15:29:04 -0400

    miltiple message

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import os, time
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# options = Options()
-# options.binary_location = "/bin/google-chrome"
-# driver = webdriver.Chrome(chrome_options = options, executable_path=r'/home/martindev/Documentos/chromedriver_linux64/chromedriver')
-
-
-chrome_options = Options()
-chrome_options.add_argument("user-data-dir=selenium")
-driver = webdriver.Chrome(options=chrome_options)
+options = Options()
+options.add_argument("user-data-dir=selenium")
+options.binary_location = "/bin/google-chrome"
+driver = webdriver.Chrome(
+    options=options, executable_path=r'/home/martindev/Documentos/chromedriver_linux64/chromedriver')
 
 
 whatsapp_url = "https://web.whatsapp.com/"
 driver.get(whatsapp_url)
-driver.maximize_window() # For maximizing window
-driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
+driver.maximize_window()  # For maximizing window
+driver.implicitly_wait(20)
 
-search_box = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]')
-search_box.send_keys("pap")
+search_box = driver.find_element(
+    By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]')
+search_box.send_keys("73883448")
 search_box.send_keys(Keys.ENTER)
-message_box = driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-for i in range(10):
-    # time.sleep(8)
-    # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'))).click()    
-    message_box.send_keys(f"Hola {i}")
-    message_box.send_keys(Keys.ENTER)
-    time.sleep(8)
+
+path_message_box = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
+
+#
+message_box = driver.find_element(By.XPATH, path_message_box)
+
+message_box.send_keys(f"funciona!!!")
+message_box.send_keys(Keys.ENTER)
+
+adjuntar = driver.find_element(
+    By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/span'
+)
+adjuntar.click()
+
+# fotos_y_videos = driver.find_element(By.XPATH,
+#                                      '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]')
+
+boton_imagen = driver.find_element(By.XPATH,
+                                   '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button')
+
+boton_imagen.send_keys("/home/martindev/Descargas/programer.jpg")
+
 
 driver.quit()
-
