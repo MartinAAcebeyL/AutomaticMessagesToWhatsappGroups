4b560742f16a4153d2ef70f7c8be1ab0b347bab8
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-06-05 15:33:50 -0400

    default srcipt

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import os
-import random
 from controllers.Whatsapp import Whatsapp
 from controllers.Drive import Drive
 from utils.utils import remove_files, show_list, read_txt
 
 
 def default():
-    items = drive.get_items(id_carpet=os.getenv('ID_CARPET'), folders=True)
-
-    for folder in items[:-1]:
+    folders = drive.get_items(id_carpet=os.getenv('ID_CARPET'), folders=True)
+    for folder in folders[:-2]:
         print(f"item: {folder.get('name')}".center(100, ' '))
-        items_carpets = drive.get_items(id_carpet=folder.get('id'))
-        # choose a txt
-        txts = [i for i in items_carpets if '.' in i.get('name')]
-        txt = random.choice(txts)
-        txt_content = drive.service.files().get_media(
-            fileId=txt.get('id')).execute().decode('utf-8')
-        whatsapp.search_contact('68638319')
-        whatsapp.send_message(txt_content)
+        items_folder = drive.get_items(id_carpet=folder.get('id'))
+        for contacto in read_txt():
+            print(f"BUSCANDO CONTACTO: {contacto}")
+            multimedia, txt = drive.multimadia_y_txts(items_folder)
+            whatsapp.search_contact(contacto)
+            whatsapp.send_message(txt)
+            for i in multimedia:
+                whatsapp.send_multimedia(i)
+            remove_files()
+            print()
 
 
 def choose_carpet():
@@ -27,9 +27,9 @@ def choose_carpet():
     show_list(principal_carpets)
     folder = input()
     folder = principal_carpets[int(folder)-1]
+    items_folder = drive.get_items(id_carpet=folder.get('id'))
     for contacto in read_txt():
         print(f"\nBUSCANDO CONTACTO: {contacto}")
-        items_folder = drive.get_items(id_carpet=folder.get('id'))
         multimedia, txt = drive.multimadia_y_txts(items_folder)
         whatsapp.search_contact(contacto)
         whatsapp.send_message(txt)
